593a680bc25da140bc0d26ef53ebb6ec89dfefbd
Matthias Klan <matthias.klan@eodc.eu>
2018-09-21 14:07:40 +0200

    fix state update in decrypt form

--- src/containers/DecryptionForm/DecryptionForm.js
+++ src/containers/DecryptionForm/DecryptionForm.js
@@ -10,10 +10,11 @@ const actions = {
 };
 
 const Component = ({ encryption, onDecrypt }) => (state, actions) =>  (
-  <SingleInputForm buttonLabel='Decrypt' placeholder="Passphrase" name="decryptPassword" onclick={() => actions.handleDecryption({encryption, onDecrypt})} type="password" value={state.password} onchange={actions.setState}  />
+  <SingleInputForm buttonLabel='Decrypt' placeholder="Passphrase" name="decryptPassword" onclick={() => actions.handleDecryption({encryption, onDecrypt})} type="password" value={state.decryptPassword} onchange={actions.setState}  />
 );
 
 export default {
   actions,
   Component
 }
+h
