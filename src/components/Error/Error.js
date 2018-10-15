e8f816729a3045272f46a36389ce3851d8610d34
Matthias Klan <matthias.klan@eodc.eu>
2018-10-15 17:49:52 +0200

    allow to click on whole notification to close error

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,8 +2,8 @@ import { h, app } from 'hyperapp';
 import './styles.css';
 
 const Error = ({ error, onClose = () => {} }) => (
-  <div className="bottom-right notification is-danger">
-    <button onclick={onClose} className="delete"></button>
+  <div onclick={onClose} className="bottom-right notification is-danger">
+    <button className="delete"></button>
     { error }
   </div>
 );
